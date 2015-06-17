d7f44460f5dc1c8e58ee00740ea1373779d7cb24
iKostov86 <i.kostov86@gmail.com>
2015-06-17 11:37:04 +0300

    working on task 2

--- Exams/C# - part 2/CSharp 2  14 Sept 2013 - Morning/2. Magic Words/MagicWords.cs
+++ Exams/C# - part 2/CSharp 2  14 Sept 2013 - Morning/2. Magic Words/MagicWords.cs
@@ -2,7 +2,54 @@
 
 class MagicWords
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
+        int maxLen = new int();
+        string[] words = new string[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            words[i] = Console.ReadLine();
+
+            if (words[i].Length > maxLen)
+            {
+                maxLen = words[i].Length;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int position = words[i].Length % (n + 1);
+            string temp = words[i];
+
+            if (position <= 0)
+            {
+                position = 0;
+            }
+            else
+            {
+                position--;
+            }
+
+            words[i] = words[position];
+            words[position] = temp;
+        }
+
+        Print (words, maxLen);
+    }
+
+    public static void Print (string[] words, int maxLen)
+    {
+        for (int i = 0; i < maxLen; i++)
+        {
+            for (int j = words.Length - 1; j >= 0 ; j--)
+            {
+                if (words[j].Length >= i)
+                {
+                    Console.WriteLine(words[j][i]);
+                }
+            }
+        }
     }
 }
