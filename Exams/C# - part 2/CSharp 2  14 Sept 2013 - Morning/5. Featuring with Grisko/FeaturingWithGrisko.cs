a6588d01b57dc49e64b8c596fed422a949663b71
iKostov86 <i.kostov86@gmail.com>
2015-06-20 17:30:53 +0300

    working on task 5

--- Exams/C# - part 2/CSharp 2  14 Sept 2013 - Morning/5. Featuring with Grisko/FeaturingWithGrisko.cs
+++ Exams/C# - part 2/CSharp 2  14 Sept 2013 - Morning/5. Featuring with Grisko/FeaturingWithGrisko.cs
@@ -8,5 +8,27 @@ public class FeaturingWithGrisko
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        string[] arr = new string[fact(input.Length)];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                
+            }
+        }
+    }
+
+    public static int fact(int x)
+    {
+        int fact = 1;
+        int i = 1;
+        while (i <= x)
+        {
+            fact = fact * i;
+            i++;
+        }
+        return fact;
     }
 }
