7eb15f90c01fbd610d7c7f62ba72f8c0b7aeefcb
iKostov86 <i02071986>
2015-08-05 14:43:05 +0300

    change version of coffee machine

--- Exams/C# - part 1/Variant 1 (2013-06-23)/01.CoffeeMachine/CoffeeMachine.cs
+++ Exams/C# - part 1/Variant 1 (2013-06-23)/01.CoffeeMachine/CoffeeMachine.cs
@@ -12,42 +12,40 @@ class CoffeeMachine
     {
         Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-        int n1 = 10;    //corresponds to 0.05 BGN
-        int n2 = 10;    //corresponds to 0.1 BGN
-        int n3 = 10;    //corresponds to 0.2 BGN
-        int n4 = 10;    //corresponds to 0.5 BGN
-        int n5 = 1;     //corresponds to 1 BGN
+        int n1 = int.Parse(Console.ReadLine());    //corresponds to 0.05 BGN
+        int n2 = int.Parse(Console.ReadLine());    //corresponds to 0.1 BGN
+        int n3 = int.Parse(Console.ReadLine());    //corresponds to 0.2 BGN
+        int n4 = int.Parse(Console.ReadLine());    //corresponds to 0.5 BGN
+        int n5 = int.Parse(Console.ReadLine());     //corresponds to 1 BGN
 
-        //price P of the selected drink
-        decimal price = 1.8m;
+        decimal developerMoney = decimal.Parse(Console.ReadLine());
+        decimal drinkPrice = decimal.Parse(Console.ReadLine());
 
-        //the amount A the developer has put in the machine
-        Console.WriteLine("Please enter amount of coins: ");
-        decimal amount = decimal.Parse(Console.ReadLine());
+        decimal change = developerMoney - drinkPrice;
+        decimal moneyInMachine = n5 * 1m + n4 * 0.5m + n3 * 0.2m + n2 * 0.1m + n1 * 0.05m; ;
 
-        while (amount < price)
+        if (change < 0) 
         {
-            Console.WriteLine("More, {} BGN needed.", (price - amount));
-            amount = decimal.Parse(Console.ReadLine());
-        }
-
-        decimal change = amount - price;
-
-        ChekCoins(ref n5, ref change, ref price, 1m);
-        ChekCoins(ref n4, ref change, ref price, 0.5m);
-        ChekCoins(ref n3, ref change, ref price, 0.2m);
-        ChekCoins(ref n2, ref change, ref price, 0.1m);
-        ChekCoins(ref n1, ref change, ref price, 0.05m);
-
-        if (change != 0)
-        {
-            Console.WriteLine("No, the machine can't give change.");
+            Console.WriteLine("More {0}", -change);
         }
         else
         {
-            Console.WriteLine("Yes, left coins are: \r\n{0} x 1BGN, \r\n{1} x 0.5BGN, \r\n{2} x 0.2BGN, \r\n{3} x 0.1BGN, \r\n{4} x 0.05BGN.", n5, n4, n3, n2, n1);
+            if (change <= moneyInMachine)
+            {
+                Console.WriteLine("Yes {0}", moneyInMachine - change);
+            }
+            else
+            {
+                Console.WriteLine("No {0}", change - moneyInMachine);
+            }
         }
     }
+
+    //ChekCoins(ref n5, ref change, ref price, 1m);
+    //ChekCoins(ref n4, ref change, ref price, 0.5m);
+    //ChekCoins(ref n3, ref change, ref price, 0.2m);
+    //ChekCoins(ref n2, ref change, ref price, 0.1m);
+    //ChekCoins(ref n1, ref change, ref price, 0.05m);
 
     static void ChekCoins(ref int n, ref decimal change, ref decimal price, decimal coin)
     {
