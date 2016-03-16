094a9b018678d198b95db6fe61e9912138808345
iKostov86 <i.kostov86sync@gmail.com>
2016-03-17 00:07:49 +0200

    12-13

--- Homeworks/C# - part 2/Methods/11-12.Polynomials/Polynomials.cs
+++ Homeworks/C# - part 2/Methods/11-12.Polynomials/Polynomials.cs
@@ -4,7 +4,7 @@ public class Polynomials
 {
     internal static void Main()
     {
-        string firstPolinomialAsStr = "x + 5x3";
+        string firstPolinomialAsStr = "-5 + x + 5x3";
         string secondPolinomialAsStr = "7x + 6x2 + 8x4 + 9x5";
         string[] firstPolinomialMembers =
             firstPolinomialAsStr
@@ -12,63 +12,136 @@ public class Polynomials
         string[] secondPolinomialMembers =
             secondPolinomialAsStr
             .Split(new char[] { ' ', '+' }, StringSplitOptions.RemoveEmptyEntries);
+        int[] firstPolinomialCoefficients = ConvertPolinomialMembersToArrayOfTheirCoefficients(firstPolinomialMembers);
+        int[] secondPolinomialCoefficients = ConvertPolinomialMembersToArrayOfTheirCoefficients(secondPolinomialMembers);
 
-
-        AddsPolynomials(polinomialA, polinomialB);
-        //SubstractPolynomials(polinomialA, polinomialB);
-        //MultipliesPolynomials(polinomialA, polinomialB);
+        //AddsPolynomials(firstPolinomialCoefficients, secondPolinomialCoefficients);
+        SubstractPolynomials(firstPolinomialCoefficients, secondPolinomialCoefficients);
+        //MultipliesPolynomials(firstPolinomialCoefficients, secondPolinomialCoefficients);
     }
 
-    public static void AddsPolynomials(int[] polynomialA, int[] polynomialB)
+    public static void AddsPolynomials(int[] firstPolinomial, int[] secondPolinomial)
     {
-        int len = Math.Max(polynomialA.Length, polynomialB.Length);
+        int firstPolinomialLen = firstPolinomial.Length;
+        int secondPolinomialLen = secondPolinomial.Length;
 
-        for (int i = 0; i < len; i++)
+        if (firstPolinomialLen > secondPolinomialLen)
         {
-            polynomialA[i] += polynomialB[i];
-        }
-
-        PrintPolynomial(polynomialA);
-    }
-
-    public static void SubstractPolynomials(int[] polynomialA, int[] polynomialB)
-    {
-        bool length = polynomialA.Length > polynomialB.Length ? true : false;
-
-        if (length)
-        {
-            for (int i = 0; i < polynomialB.Length; i++)
+            for (int i = 0; i < secondPolinomialLen; i++)
             {
-                polynomialA[i] -= polynomialB[i];
+                firstPolinomial[i] += secondPolinomial[i];
             }
 
-            PrintPolynomial(polynomialA);
+            PrintPolynomial(firstPolinomial);
         }
         else
         {
-            for (int i = 0; i < polynomialA.Length; i++)
+            for (int i = 0; i < firstPolinomialLen; i++)
             {
-                polynomialB[i] -= polynomialA[i];
+                secondPolinomial[i] += firstPolinomial[i];
             }
 
-            PrintPolynomial(polynomialB);
+            PrintPolynomial(secondPolinomial);
         }
+
     }
 
-    public static void MultipliesPolynomials(int[] polynomialA, int[] polynomialB)
+    public static void SubstractPolynomials(int[] firstPolinomial, int[] secondPolinomial)
     {
-        int length = polynomialA.Length + polynomialB.Length;
-        int[] polynomial = new int[length - 1];
-
-        for (int i = 0; i < polynomialA.Length; i++)
+        for (int i = 0, len = secondPolinomial.Length; i < len; i++)
         {
-            for (int j = 0; j < polynomialB.Length; j++)
+            secondPolinomial[i] *= -1;
+        }
+
+        AddsPolynomials(firstPolinomial, secondPolinomial);
+    }
+
+    public static void MultipliesPolynomials(int[] firstPolinomial, int[] secondPolinomial)
+    {
+        int len = firstPolinomial.Length + secondPolinomial.Length;
+        int[] polynomial = new int[len - 1];
+
+        for (int i = 0, firstPolinomialLen = firstPolinomial.Length; i < firstPolinomialLen; i++)
+        {
+            for (int j = 0, secondPolinomialLen = secondPolinomial.Length; j < secondPolinomialLen; j++)
             {
-                polynomial[i + j] += (polynomialA[i] * polynomialB[j]);
+                polynomial[i + j] += (firstPolinomial[i] * secondPolinomial[j]);
             }
         }
 
         PrintPolynomial(polynomial);
+    }
+
+    public static int[] ConvertPolinomialMembersToArrayOfTheirCoefficients(string[] polinomialMembers)
+    {
+        var polinomialMembersCount = polinomialMembers.Length;
+        var polinomialCoefficients = new int[polinomialMembersCount];
+        var container = new int[polinomialMembersCount];
+
+        for (int i = 0; i < polinomialMembersCount; i++)
+        {
+            string polinomialMember = polinomialMembers[i];
+            int polinomialMemberLen = polinomialMember.Length;
+            string valueAsStr = string.Empty;
+            string indexAsStr = string.Empty;
+            bool isValueSet = false;
+            bool hasIndex = false;
+            int value = new int();
+            int index = new int();
+
+            for (int j = 0; j < polinomialMemberLen; j++)
+            {
+                char polinomialMemberSymbol = polinomialMember[j];
+
+                if (polinomialMemberSymbol == '-')
+                {
+                    valueAsStr += '-';
+                }
+                else if (polinomialMemberSymbol != 'x')
+                {
+                    if (isValueSet)
+                    {
+                        indexAsStr += polinomialMemberSymbol;
+                    }
+                    else
+                    {
+                        valueAsStr += polinomialMemberSymbol;
+                    }
+                }
+                else
+                {
+                    isValueSet = true;
+                    hasIndex = true;
+                    value = int.Parse(valueAsStr != string.Empty ? valueAsStr : "1");
+                }
+            }
+
+            if (!isValueSet)
+            {
+                value = int.Parse(valueAsStr);
+            }
+
+            if (hasIndex)
+            {
+                index = int.Parse(indexAsStr != string.Empty ? indexAsStr : "1");
+            }
+            else
+            {
+                index = 0;
+            }
+
+            if (polinomialCoefficients.Length <= index)
+            {
+                Array.Copy(polinomialCoefficients, container, polinomialCoefficients.Length);
+                polinomialCoefficients = new int[index + 1];
+                Array.Copy(container, polinomialCoefficients, container.Length);
+                container = new int[polinomialCoefficients.Length];
+            }
+
+            polinomialCoefficients[index] = value;
+        }
+
+        return polinomialCoefficients;
     }
 
     public static void PrintPolynomial(int[] polynomial)
@@ -90,7 +163,8 @@ public class Polynomials
                     sign = negativeSign;
                 }
 
-                Console.Write("{0}x^{1} {2}  ", polynomial[i], i, sign);
+                Console.Write("{0}x^{1} + ", polynomial[i], i);
+                //Console.Write("{0}x^{1} {2} ", polynomial[i], i, sign);
             }
             else
             {
