c6b14525b4d0793d04a60a1496db78e227f5288b
iKostov86 <i.kostov86sync@gmail.com>
2016-10-09 11:51:32 +0300

    Intro

--- Homeworks/C# - part 1/IntroductionToProgramming/15. Age/Age.cs
+++ Homeworks/C# - part 1/IntroductionToProgramming/15. Age/Age.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 public class Age
 {
     static void Main(string[] args)
     {
-        string birthdayAsStr = "05.16.1994";
-        string birthdayAsStr2 = "07.02.1986";
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        string[] birthdays = { "07.02.1986", "05.16.1994", "10.01.1994" };
         string format = "MM.dd.yyyy";
 
         //DateTime dateOfBirth = DateTime.ParseExact(birthdayAsStr, format, CultureInfo.InvariantCulture);
-        //DateTime present = DateTime.ParseExact("02.04.2016", format, CultureInfo.InvariantCulture);
+        //DateTime present = DateTime.ParseExact("04.01.2016", format, CultureInfo.InvariantCulture);
 
         DateTime dateOfBirth = DateTime.ParseExact(Console.ReadLine(), format, CultureInfo.InvariantCulture);
         DateTime present = DateTime.Now;
 
         TimeSpan difference = present.Subtract(dateOfBirth);
+        Console.WriteLine(difference.TotalDays / 365.25);
         //int age = DateTime.MinValue.AddDays(difference.Days).Year - 1;
         int age = (new DateTime(1, 1, 1) + difference).Year - 1;
 
-        Console.WriteLine(age - 1);
 
-        present = present.AddYears(10);
-        difference = present - dateOfBirth;
-        //age = DateTime.MinValue.AddDays(difference.Days).Year - 1;
-        age = (new DateTime(1, 1, 1) + difference).Year - 1;
-
-        Console.WriteLine(age - 1);
+        Console.WriteLine(age);
+        Console.WriteLine(age + 10);
     }
 }
